84017c505948536300e0237de07052bc5d3c14c3
David Stančík <stancik.david@gmail.com>
2020-03-20 16:36:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -30,18 +30,33 @@
 			m.addLayer(layer);
 			layer.enable();
 
+			//Tady je začátek tvoření bodu na mapě. Tedy tady bude začátek for loopu
+
 			var card = new SMap.Card();
+
 			card.getHeader().classList.add("clearfix");
+
+			//Tady dochází k vytvoření popupu s daty - záhlaví (název, adresa, CTA - zavolá na telefon, proklik na web):
+
 			card.getHeader().innerHTML = "<div class='clearfix'><img class='hto' src='images/hto.svg'><div class='algn'><h1 class='hto-heading'>HTO Praha 6</h1><h2 class='hto-adress'>Adresa 1<br>000 01<br>Praha</h2></div></div><div class='clearfix'><p class='call'>Objednat se</p><div class='link-bg'><img src='images/link.svg' class='link'></div></div>";
+
+			//Tady dochází k vytvoření barometru. Zatím dva řádky:
+
 			card.getHeader().innerHTML += "<div class='clearfix loadings'><div class='clearfix'><p class='b-type'>A+</p><div class='b-load'><div class='b-state b-come'></div></div></div><div class='clearfix'><p class='b-type'>A-</p><div class='b-load'><div class='b-state b-empty'></div></div></div></div>";
+
+			//Titulek bodu na mapě. Asi název HTO:
 
 			var options = { 
 			    title: "HTO Praha"
 			};
 
+			//Vytvoření samotného bodu, na který je uchycen popup: ("Benešov je ID, to je třeba udělat unikátní a samozřejmě tam přijde ID HTO/nemocnice"):
+
 			var marker = new SMap.Marker(SMap.Coords.fromWGS84(14.6791711, 49.7861753), "Benešov", options);
 			marker.decorate(SMap.Marker.Feature.Card, card);
 			layer.addMarker(marker);
+
+			//Tady je konec tvoření bodu na mapě. Tedy tady bude konec for loopu
 
 			var options = {
 				enableHighAccuracy: true,
